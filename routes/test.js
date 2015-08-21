db057da62c6807edb75463164a6da980dea337a1
thmckenzie <thm2@williams.edu>
2015-08-21 15:02:23 -0400

    anonymous

--- routes/test.js
+++ routes/test.js
@@ -12,10 +12,10 @@ module.exports.sayHi = function (req, res){
       "<team_domain: input type='text' name='team_domain' value='timeinc'></input><br>" +
       "<service_id: input type='text' name='service_id' value='9435179584'></input><br>" +
       "<channel_id: input type='text' name='channel_id' value='C03BGDH2U'></input><br>" +
-      "<channel_name: input type='text' name='channel_name' value ='si_golf-dev-team'></input></br>" +
+      "<channel_name: input type='text' name='channel_name' value ='*****'></input></br>" +
       "<timestamp: input type='text' name='timestamp' value='1440171763.000010'></input><br>" +
       "<user_id: input type='text' name='user_id' value='U039HD0HP'></input><br>" +
-      "<user_name: input type='text' name='user_name' value='aaron.marasco'></input><br>" +
+      "<user_name: input type='text' name='user_name' value='*****'></input><br>" +
       "text: input type='text' name='text' value ='stuff test'></input><br>" +
       "trigger_word: input type='text' name='trigger_word' value='stuff'></input></form>");
 };
